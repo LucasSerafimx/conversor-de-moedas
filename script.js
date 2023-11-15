37e74d55fe331406e0e04ed6958cb367acd55055
LucasSerafimx <lucas.serafimx@gmail.com>
2023-11-15 16:58:52 -0300

    Alteração Time Alert

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ var cotacaoDoEuro = 5.27;
 var valorEmRealDolar = cotacaoDoDolar * valorEmDolar;
 var valorEmRealEuro = cotacaoDoEuro * valorEmEuro;
 
-alert("O dolar estar R$" + valorEmRealDolar.toFixed(2) + "hoje.");
-
-alert("O Euro estar R$" + valorEmRealEuro.toFixed(2) + " hoje.");
-
 //toFixed é para limitar casas decimais
+//Aumentando tempo do alertar
+
+setTimeout(function() {
+    alert("O dolar estar R$" + valorEmRealDolar.toFixed(2) + "hoje.");
+  }, 5000);
+setTimeout(function() {
+    alert("O Euro estar R$" + valorEmRealEuro.toFixed(2) + " hoje.");
+  }, 5000);
